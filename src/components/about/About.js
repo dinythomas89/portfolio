d513e66087db34be2821bce366071209e2054157
Fendous <fendousteam@gmail.com>
2023-08-29 10:55:34 +0200

    removed space ani from about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,11 +1,9 @@
 import { BsEmojiWinkFill } from "react-icons/bs";
+import { Slide } from "react-reveal";
 import "./about.scss";
 import AnimatedElement from "../AnimatedElement";
-import myImage from "../../assests/myImage.jpeg";
-import ClickHere from "../animations/clickHere/ClickHere";
 import backgroundEarth from "../../assests/earth2.png";
-import earthImage from "../../assests/earth3.png";
-import satellite from "../../assests/satellite2.png";
+import myImage from "../../assests/Diny.png";
 
 const About = () => {
   return (
@@ -14,47 +12,40 @@ const About = () => {
       id="about"
       style={{ backgroundImage: `url(${backgroundEarth})` }}
     >
-      <h3>Get to know me!</h3>
       <div>
-        <div className="click-here-wrapper">
-          <ClickHere />
-        </div>
-        <div className="card">
-          <div className="imgBox">
-            <img src={myImage} alt="" />
-          </div>
-          <div className="details">
+        <div className="left">
+          <h2>A little about me!</h2>
+          <div>
             <AnimatedElement animationClass="para-animation">
               <p>
-                I am a Frontend focused Full Stack Web Developer with a
-                Bachelor's degree in computer science, living in Denmark for the
-                past 9 years. I enjoy finding solutions to problems, taking deep
-                dives into the how/why things work, and continuously improving
-                my skills.
+                Greetings! I'm a Frontend-focused Full Stack Web Developer with
+                a Computer Science degree, and Denmark has been my home for the
+                past 9 years.
               </p>
               <p>
-                In my free time, I enjoy music (great singer too, if no one is
-                around{" "}
-                <span>
-                  <BsEmojiWinkFill />
-                </span>
-                ) and novels.
+                I love solving problems and digging into how things work. My
+                journey is all about continuously improving my skills and
+                finding creative solutions.
               </p>
               <p>
-                I'm open to job opportunities where I can contribute, learn and
-                grow. If you have a good opportunity that matches my skills and
-                experience then don't hesitate to contact me.
+                When I'm not coding, you'll catch me enjoying music (I might
+                even sing a bit when no one's around <BsEmojiWinkFill />) and
+                diving into novels.
+              </p>
+              <p>
+                I'm on the lookout for job opportunities that let me learn,
+                grow, and contribute. If you've got an exciting opportunity that
+                matches my skills, feel free to get in touch. Let's make
+                something great together!
               </p>
             </AnimatedElement>
           </div>
         </div>
-        {/* <div className="earth"></div> */}
-      </div>
-      <div className="earth-image">
-        <img src={earthImage} />
-      </div>
-      <div className="satellite">
-        <img src={satellite} />
+        <div className="right">
+          <Slide top>
+            <img src={myImage} alt="" />
+          </Slide>
+        </div>
       </div>
     </div>
   );
